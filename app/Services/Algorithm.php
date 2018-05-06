52ab18a396f516c9b03c32dfd4a24c46d4787f29
Konstantina_Avg <konavge93@gmail.com>
2018-05-06 11:26:22 +0300

    merge d

---
+++ app/Services/Algorithm.php
@@ -0,0 +1,275 @@
+<?php
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: MidoriKage
+ * Date: 05-May-18
+ * Time: 12:46 PM
+ */
+
+namespace App\Services;
+
+use App\Services\Dejkstra\Dejkstra;
+use App\Services\Dejkstra\GraphHandler;
+use App\Services\Dejkstra\Node;
+use Illuminate\Support\Facades\Log;
+
+class Algorithm
+{
+    private $calculator;
+    private $villages = array();
+    private $tracks = array();
+
+    function addVillages(array $village)
+    {
+        $this->villages = $village;
+    }
+
+    function addTracks(array $track)
+    {
+        $this->tracks = $track;
+    }
+
+    function execute($data)
+    {
+        $this->calculator = new DistanceCalculator();
+
+        $start = null;
+        $end = null;
+
+        foreach ($this->villages as $village) {
+            if ($village->getType() == "start") {
+                $start = $village;
+                $start->setVisitedTrue();
+            } else if ($village->getType() == "end") {
+                $end = $village;
+            }
+        }
+
+        $currentVillage = $start;
+        $flag = false;
+
+        $isGoingBack = false;
+        $useLocator = false;
+        $lookingFor = null;
+
+        $path = [];
+        $secondaryResults = [];
+        do {
+            if (!$useLocator) {
+                $max = count($currentVillage->getAvailableRoutes());
+                $val = rand(0, $max - 1);
+                $villageName = $currentVillage->getAvailableRoutes()[$val];
+                $vilObj = $this->locateVillage($villageName);
+
+                Log::info("CurrentArea : " . $currentVillage->getName());
+                Log::info("NextArea : " . $vilObj->getName());
+
+                $currentVillage->setVisitedTrue();
+
+                if ($vilObj->isVisited()) {
+                    Log::info("Canceled");
+                    $areAllNodesVisited = $this->areAllSurroundingVisited($currentVillage->getAvailableRoutes());
+                    if ($areAllNodesVisited) {
+                        $useLocator = true;
+                    }
+                    continue;
+                }
+
+                array_push($path, $currentVillage);
+                $currentVillage = $vilObj;
+            } else {
+                $innerFlag = true;
+                $lastName = $currentVillage->getName();
+                do {
+                    $result = array();
+                    $locatedVillages = $this->villagesNeeded();
+                    if (empty($locatedVillages)) {
+                        Log::info("locatedVillages was empty");
+                        if ($lastName === $end->getName()) {
+                            Log::info("current was last");
+                            Log::info("currentVillage name " . $currentVillage->getName());
+                            Log::info("endVillage name " . $end->getName());
+                            $flag = true;
+                            break;
+                            //done
+                            //miracle
+                        } else {
+                            Log::info("current was NOT last");
+                            $result = $this->getTargetedPath($lastName, $end->getName());
+                            Log::info("currentVillage name " . $lastName);
+                            Log::info("endVillage name " . $end->getName());
+                            array_push($secondaryResults, $result);
+                            $flag = true;
+                            break;
+                        }
+                    }
+                    Log::info("need to go from " . $lastName . " to " . $locatedVillages[0]->getName());
+                    $result = $this->getTargetedPath($lastName, $locatedVillages[0]->getName());
+                    $lastName = $locatedVillages[0]->getName();
+                    array_push($secondaryResults, $result);
+                } while ($innerFlag);
+            }
+        } while (!$flag);
+
+        $distance = 0;
+        $item = $this->locateVillage($path[0]->getName());
+        $strPath = '{' . $path[0]->getName() . ',' . $item->getLat() . ',' . $item->getLon() . '}' . ":";
+        for ($i = 1; $i < count($path); $i++) {
+            $distance += $this->calculator->calculateDistance($path[$i - 1]->getLat(), $path[$i - 1]->getLon(),
+                $path[$i]->getLat(), $path[$i]->getLon())["meters_distance"];
+            $item = $this->locateVillage($path[$i]->getName());
+            $strPath = $strPath . '{' . $path[$i]->getName() . ',' . $item->getLat() . ',' . $item->getLon() . '}' . ":";
+        }
+        $lastVal = "";
+
+        foreach ($secondaryResults as $result) {
+            foreach ($result as $inner) {
+                if ($lastVal === $inner->getName())
+                    continue;
+                $item = $this->locateVillage($inner->getName());
+                $strPath = $strPath . '{' . $inner->getName() . ',' . $item->getLat() . ',' . $item->getLon() . '}' . ":";
+                $distance += $inner->getPotential();
+                $lastVal = $inner->getName();
+            }
+        }
+
+        return [
+            'distance' => $distance,
+            'path' => $strPath
+        ];
+    }
+
+    public function getTargetedPath($from, $to, $fromPath = array(), $endFlag = false)
+    {
+        $graph = new GraphHandler();
+
+        $nodes = [];
+
+        foreach ($this->villages as $village) {
+            $node = new Node($village->getName());
+            if ($village->getType() === "end" && $this->lastNodeOneTimeStatus && $skipEnd) {
+                Log::info("Skipping ending point(1)");
+                continue;
+            }
+
+            $node->addChild($village);
+            $nodes[$village->getName()] = $node;
+        }
+        foreach ($nodes as $node) {
+            $villageObj = $this->locateVillage($node->getName());
+            foreach ($villageObj->getAvailableRoutes() as $route) {
+                $villageTarget = $this->locateVillage($route);
+                $cal = $this->calculator->calculateDistance($villageObj->getLat(), $villageObj->getLon(),
+                    $villageTarget->getLat(), $villageTarget->getLon());
+
+                $node->connect($nodes[$villageTarget->getName()], $cal['meters_distance']);
+            }
+        }
+
+        foreach ($nodes as $node) {
+            $graph->add($node);
+        }
+
+        $de = new Dejkstra($graph);
+        $de->setStartingVertex($nodes[$from]);
+        $de->setEndingNode($nodes[$to]);
+        Log::info("About to solve");
+        return $de->solve();
+    }
+
+    public function getTargetedPath2($from, $to, $fromPath = array(), $endFlag = false)
+    {
+        $unvisitedNodes = array();
+        $unvisitedNodes = collect($this->villages)->flatMap(function ($item, $key) {
+            return [$item->getName() => $item];
+        })->toArray();
+//        foreach ($this->villages as $village) {
+//            $unvisitedNodes[$village->getName()] = $village;
+////            array_push($unvisitedNodes, $village, $village->getName());
+//        }
+
+        $nodes = array_keys($unvisitedNodes);
+
+        $startNode = $unvisitedNodes[$from->getName()];
+        $currentNode = $startNode;
+        $paths = [];
+
+        for (; ;) {
+            $myLat = $currentNode->getLat();
+            $myLon = $currentNode->getLon();
+            foreach ($currentNode->getAvailableRoutes() as $availableRoute) {
+                $villageObj = null;
+                foreach ($this->villages as $village) {
+                    if ($village->getName() === $availableRoute) {
+                        $villageObj = $village;
+                    }
+                }
+
+                if (is_null($villageObj)) {
+                    throw new \InvalidArgumentException();
+                }
+
+                $distance = $this->calculator->calculateDistance($myLat, $myLon,
+                    $villageObj->getLat(), $villageObj->getLon());
+
+                dd($distance);
+            }
+            dd($currentNode);
+        }
+
+        $totalMap = [];
+
+
+        $distance = [];
+        $parent = [];
+        $visit = [];
+        dd("fill");
+    }
+
+    public function areAllSurroundingVisited(array $names): bool
+    {
+        foreach ($names as $name) {
+            $vil = $this->locateVillage($name);
+            if (!$vil->isVisited()) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private function villagesNeeded()
+    {
+        $toReturn = array();
+        foreach ($this->villages as $village) {
+            if (!$village->isVisited()) {
+                if ($village->getType() === "end" && $this->lastNodeOneTimeStatus) {
+                    continue;
+                }
+                array_push($toReturn, $village);
+            }
+        }
+
+        return $toReturn;
+    }
+
+    private function wasTheRouteFulfilled(): bool
+    {
+        foreach ($this->villages as $village) {
+            if (!$village->isVisited()) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private function locateVillage(string $name): VillageSchematics
+    {
+        foreach ($this->villages as $village) {
+            if ($village->getName() === $name) {
+                return $village;
+            }
+        }
+        return null;
+    }
+}
---
+++ app/Services/DistanceCalculator.php
@@ -0,0 +1,97 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: orestis
+ * Date: 5/5/2018
+ * Time: 1:22 μμ
+ */
+
+namespace App\Services;
+
+define('GOOGLE_DEST_URL', 'https://maps.googleapis.com/maps/api/distancematrix/json?');
+
+class DistanceCalculator
+{
+    public function ddd($lat_route, $lon_route, $des_lat, $des_lon)
+    {
+        $matrix['41.092083']['23.541016']['41.068238']['23.390686'] = 15126;
+        $matrix['41.092083']['23.541016']['41.05868']['23.457547'] = 9423;
+        $matrix['41.092083']['23.541016']['41.020032']['23.520701'] = 9140;
+        $matrix['41.068238']['23.390686']['41.092083']['23.541016'] = 14183;
+        $matrix['41.068238']['23.390686']['41.05832']['23.424134'] = 5144;
+        $matrix['41.05868']['23.457547']['41.05832']['23.424134'] = 3064;
+        $matrix['41.05868']['23.457547']['41.020431']['23.483293'] = 7040;
+        $matrix['41.05868']['23.457547']['41.092083']['23.541016'] = 9373;
+        $matrix['41.05868']['23.457547']['41.016434']['23.434656'] = 5511;
+        $matrix['41.020032']['23.520701']['41.092083']['23.541016'] = 9100;
+        $matrix['41.020032']['23.520701']['41.020431']['23.483293'] = 3218;
+        $matrix['41.020032']['23.520701']['40.988154']['23.516756'] = 4488;
+        $matrix['41.020032']['23.520701']['41.003545']['23.559196'] = 4622;
+        $matrix['41.05832']['23.424134']['41.068238']['23.390686'] = 5144;
+        $matrix['41.05832']['23.424134']['41.05868']['23.457547'] = 3064;
+        $matrix['41.05832']['23.424134']['41.016434']['23.434656'] = 6272;
+        $matrix['41.020431']['23.483293']['41.05868']['23.457547'] = 7275;
+        $matrix['41.020431']['23.483293']['41.016434']['23.434656'] = 5735;
+        $matrix['41.020431']['23.483293']['41.020032']['23.520701'] = 3251;
+        $matrix['41.016434']['23.434656']['41.05832']['23.424134'] = 6399;
+        $matrix['41.016434']['23.434656']['41.020431']['23.483293'] = 5833;
+        $matrix['41.016434']['23.434656']['41.014645']['23.457354'] = 2466;
+        $matrix['40.988154']['23.516756']['41.014645']['23.457354'] = 6821;
+        $matrix['40.988154']['23.516756']['41.020032']['23.520701'] = 4019;
+        $matrix['40.988154']['23.516756']['41.003545']['23.559196'] = 4734;
+        $matrix['41.003545']['23.559196']['40.988154']['23.516756'] = 4784;
+        $matrix['41.003545']['23.559196']['41.020032']['23.520701'] = 4622;
+        $matrix['41.014645']['23.457354']['41.016434']['23.434656'] = 2427;
+        $matrix['41.014645']['23.457354']['40.988154']['23.516756'] = 6821;
+        $matrix['41.016434']['23.434656']['41.05868']['23.457547'] = 5511;
+
+        return $matrix[strval($lat_route)][strval($lon_route)][strval($des_lat)][strval($des_lon)];
+    }
+
+    public function calculateDistance($lat_route, $lon_route, $des_lat, $des_lon)
+    {
+        return ['meters_distance' => $this->ddd($lat_route, $lon_route, $des_lat, $des_lon)];
+//        $distance = $this->setupUrl($lat_route, $lon_route, $des_lat, $des_lon);
+//        $distanceDec = json_decode($distance);
+//
+//        if ($distanceDec->status != "OK") {
+//            dd($distance);
+//        }
+//
+//        $result = $this->formatResult($distanceDec->rows);
+//
+//        return $result;
+    }
+
+    private function curlGoogleDistanceApi($url)
+    {
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+
+        $result = curl_exec($ch);
+        curl_close($ch);
+
+        return $result;
+    }
+
+    private function setupUrl($lat_route, $lon_route, $des_lat, $des_lon)
+    {
+        $url = GOOGLE_DEST_URL . "origins=$lat_route,$lon_route&destinations=$des_lat,$des_lon&key=AIzaSyDFb1m9lj3C-eBveJPpcR5YO1LcAWI53mE";
+
+        $result = $this->curlGoogleDistanceApi($url);
+        return $result;
+    }
+
+    private function formatResult($distance)
+    {
+        $parseDist = $distance[0]->elements[0]->distance;
+
+        $distArr = [
+            'km_distance' => $parseDist->text,
+            'meters_distance' => $parseDist->value
+        ];
+
+        return $distArr;
+    }
+}
